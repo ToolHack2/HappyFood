197b0375dfcb3bb8be91bf74c23f63f56055191b
vanminhphan2 <vanminhphan2@gmail.com>
2018-08-05 22:08:52 +0700

    update register

--- SMARTFOODY/app/src/main/java/com/example/user/smartfoody/Login/Register.java
+++ SMARTFOODY/app/src/main/java/com/example/user/smartfoody/Login/Register.java
@@ -103,18 +103,18 @@ public class Register extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
         FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
-        mUser.getIdToken(true)
-                .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
-                    public void onComplete(@NonNull Task<GetTokenResult> task) {
-                        if (task.isSuccessful()) {
-                            String idToken = task.getResult().getToken();
-                            // Send token to your backend via HTTPS
-                            // ...
-                        } else {
-                            // Handle error -> task.getException();
-                        }
-                    }
-                });
+//        mUser.getIdToken(true)
+//                .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+//                    public void onComplete(@NonNull Task<GetTokenResult> task) {
+//                        if (task.isSuccessful()) {
+//                            String idToken = task.getResult().getToken();
+//                            // Send token to your backend via HTTPS
+//                            // ...
+//                        } else {
+//                            // Handle error -> task.getException();
+//                        }
+//                    }
+//                });
 
 
         //
